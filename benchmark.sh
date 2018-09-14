f7273f2fc52cfc11872add2a7c646bc0eabdd8b5
Jakub Bednar <jakub.bednar@gmail.com>
2018-09-14 12:03:42 +0200

    New versions of inch

--- benchmark.sh
+++ benchmark.sh
@@ -4,101 +4,50 @@ singleQuery=""
 multipleQuery=""
 database=""
 
-echo "###################################################################################"
-echo
-echo "Single property query, chunking 100, repeat 1x, JSON"
-echo "----------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 1 -hide true
-echo 
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 1 -hide true
-echo
-echo "Single property query, synchronous, repeat 1x, JSON"
-echo "---------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 1 -hide true -chunking 0
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 1 -hide true -chunking 0
-echo
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo
-echo "Single property query, chunking 100, repeat 10x, JSON"
-echo "-----------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 10 -hide true
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 10 -hide true
-echo
-echo "Single property query, synchronous, repeat 10x, JSON"
-echo "----------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 10 -hide true -chunking 0
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 10 -hide true -chunking 0
-echo
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo
-echo "Single property query, chunking 100, repeat 50x, JSON"
-echo "-----------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 50 -hide true
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 50 -hide true
-echo
-echo "Single property query, synchronous, repeat 50x, JSON"
-echo "----------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 50 -hide true -chunking 0
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${singleQuery}" -database ${database} -repeat 50 -hide true -chunking 0
-echo
-echo "###################################################################################"
-echo
-echo "Multiple property query, chunking 100, repeat 1x, JSON"
-echo "------------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 1 -hide true -limits "100,500,1000,5000,10000,50000"
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 1 -hide true -limits "100,500,1000,5000,10000,50000"
-echo
-echo "Multiple property query, synchronous, repeat 1x, JSON"
-echo "------------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 1 -hide true -limits "100,500,1000,5000,10000,50000" -chunking 0
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 1 -hide true -limits "100,500,1000,5000,10000,50000" -chunking 0
-echo
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo
-echo "Multiple property query, chunking 100, repeat 10x, JSON"
-echo "-------------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 10 -hide true -limits "100,500,1000,5000,10000,50000"
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 10 -hide true -limits "100,500,1000,5000,10000,50000"
-echo
-echo "Multiple property query, synchronous, repeat 10x, JSON"
-echo "-------------------------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 10 -hide true -limits "100,500,1000,5000,10000,50000" -chunking 0
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 10 -hide true -limits "100,500,1000,5000,10000,50000" -chunking 0
-echo
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo
-echo "Multiple property query, chunking 100, repeat 50x, JSON"
-echo "--------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 50 -hide true -limits "100,500,1000,5000,10000,50000"
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 50 -hide true -limits "100,500,1000,5000,10000,50000"
-echo
-echo "Multiple property query, synchronous, repeat 50x, JSON"
-echo "--------------------------------------"
-echo
-java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 50 -hide true -limits "100,500,1000,5000,10000,50000" -chunking 0
-echo
-java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "${multipleQuery}" -database ${database} -repeat 50 -hide true -limits "100,500,1000,5000,10000,50000" -chunking 0
-echo
+chunking=100
 
+query () {
+
+    echo "$1"
+    echo
+    echo  " - chunking: $2"
+    echo  " - repeat: $3x"
+    echo  " - format: $4"
+    echo  " - limits: $6"
+#    echo  " - query: $5"
+
+    echo
+    java -cp inch-java-2.12.jar org.influxdb.tool.benchmark.QueryComparator -query "$5" -database ${database} -repeat $3 -hide true -chunking $2 -format $4 -limits "$6"
+    echo
+    java -cp inch-java-2.13.jar org.influxdb.tool.benchmark.QueryComparator -query "$5" -database ${database} -repeat $3 -hide true -chunking $2 -format $4 -limits "$6"
+    echo
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+    echo
+
+   return 0
+}
+
+echo "################################"
+echo "#             JSON             #"
+echo "################################"
+
+query "Single property query" ${chunking} 1 "JSON" "${singleQuery}" "100,1000,10000"
+query "Single property query" ${chunking} 10 "JSON" "${singleQuery}" "100,1000,10000"
+query "Single property query" ${chunking} 50 "JSON" "${singleQuery}" "100,1000,10000"
+
+query "Multiple property query" ${chunking} 1 "JSON" "${multipleQuery}" "100,1000,10000"
+query "Multiple property query" ${chunking} 10 "JSON" "${multipleQuery}" "100,1000,10000"
+query "Multiple property query" ${chunking} 50 "JSON" "${multipleQuery}" "100,1000,10000"
+
+echo "################################"
+echo "#           MSGPACK            #"
+echo "################################"
+
+query "Single property query" ${chunking} 1 "MSGPACK" "${singleQuery}" "100,1000,10000"
+query "Single property query" ${chunking} 10 "MSGPACK" "${singleQuery}" "100,1000,10000"
+query "Single property query" ${chunking} 50 "MSGPACK" "${singleQuery}" "100,1000,10000"
+
+query "Multiple property query" ${chunking} 1 "MSGPACK" "${multipleQuery}" "100,1000,10000"
+query "Multiple property query" ${chunking} 10 "MSGPACK" "${multipleQuery}" "100,1000,10000"
+query "Multiple property query" ${chunking} 50 "MSGPACK" "${multipleQuery}" "100,1000,10000"
 
